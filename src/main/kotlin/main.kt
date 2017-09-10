10e9ac908323f7417b997c269d40c431a4895a69
Peter Helstrup Jensen <peterhj@gmail.com>
2017-09-10 13:59:56 +0200

    crossover is better when using pick

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -61,10 +61,7 @@ class Specimen {
     }
 
     fun crossover(pool: Array<Specimen>, crossoverfreq: Double, strategy: Int) {
-        val mate = when (strategy) {
-            0 -> pool[(random() * pool.size).toInt()]
-            else -> Specimen.pick(pool)
-        }
+        val mate = Specimen.pick(pool)
         for (i in 0 until data.size) if (random() < crossoverfreq)
             data[i] = mate.data[i]
     }
@@ -103,19 +100,19 @@ fun genetic() {
     val mutatePropDecay = 0.9
     val mutateFreq = 0.04    // Prop that character will mutate
 
-    val crossoverProp = 0.38 // Prop that specimen will crossover
-    val crossoverFreq = 0.27 // Prop that character will crossover
+    val crossoverProp = 0.48 // Prop that specimen will crossover
+    val crossoverFreq = 0.37 // Prop that character will crossover
 
     // val target = stringToIntArray("to be or not to be that is the question")
     // val target = stringToIntArray("the time when the computer was a gray and tiresome box on the floor is a long time ago a longer time ago than far far away in a galaxy of the guardians")
     val target = stringToIntArray("the hazards of visiting this island were legendary it was not just the hostility of the best anchorage on the island nor the odd accidents known to befall ships and visitors the whole of the island was enshrouded in the peculiar magic of the others kennit had felt it tugging at him as he and gankis followed the path that led from deception cove to the treasure beach for a path seldom used its black gravel was miraculously clean of fallen leaves or intruding plant life about them the trees dripped the secondhand rain of last night's storm onto fern fronds already burdened with crystal drops")
-    val poolsize = 1_000         // Poolsize
+    val poolsize = 20_000         // Poolsize
 
     val plot = true
-    val timeInSeconds = 20
+    val timeInSeconds = 40
 
     val colors = plotColors.keys
-    val selectionStrategy = listOf(0, 1)
+    val selectionStrategy = listOf(1, 0)
     val poolsizes = linspace(10_000.0, 10_000.0, 1).toList()
     val crossoverProps = linspace(0.1, 0.9, 3)
     val crossoverFreqs = linspace(0.1, 0.9, 3).toList()
@@ -134,11 +131,11 @@ private fun geneticAlgorithm(poolsize: Int, targetString: IntArray, mutateProp: 
     var pool = Array(poolsize) { Specimen(targetString) }
     while (Duration.between(starts, Instant.now()).seconds < timeInSeconds) {
         when (strategy) {
-            1 -> {
+            0 -> {
                 Specimen.computeWheel(pool)
                 val poolList = pool.asList().parallelStream().map { Specimen.pick(pool) }.collect(Collectors.toList())
                 poolList.parallelStream().forEach {
-                    if (random() < crossoverProp) it.crossover(pool, crossoverFreq, 0)
+                    if (random() < crossoverProp) it.crossover(pool, crossoverFreq)
                     if (random() < mutateProp) it.mutate(mutateFreq)
                     it.computeFitness()
                 }
